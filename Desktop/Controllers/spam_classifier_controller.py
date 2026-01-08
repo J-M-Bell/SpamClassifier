0d22aad583922001f44804f2b987e6c0d466163c
JM Bell <justinmbell1997@gmail.com>
2026-01-08 12:04:57 -0600

    Controllers (Desktop) - added documentation to controller and controller test files

--- Desktop/Controllers/spam_classifier_controller.py
+++ Desktop/Controllers/spam_classifier_controller.py
@@ -4,15 +4,38 @@ from Models.spam_classifier_model import SpamClassifierModel
 import resources
 
 class SpamClassifierController(QWidget):
+    """
+    A class that acts as the Controller for
+    the spam_classifier_view
+    """
     def __init__(self):
+        """
+        constructor for SpamClassifierController
+        
+        :param self: SpamClassifierController - The SpamClassifierController object
+        """
+
+        # Start up window
         super().__init__()
         uic.loadUi("./Desktop/Views/spam_classifier_view.ui", self)
         self.setWindowTitle("Spam Classifier")
-        self.submitButton.clicked.connect(self.makePrediction)
-        self.resetButton.clicked.connect(self.resetScreen)
+
+        #Instantiate spam model
         self.model = SpamClassifierModel()
 
+        #Assign functionalities to UI elements
+        self.submitButton.clicked.connect(self.makePrediction)
+        self.resetButton.clicked.connect(self.resetScreen)
+
     def makePrediction(self):
+        """
+        ui function to change the text for the 
+        resultLabel between spam or ham
+        
+        :param self: SpamClassifierController - The SpamClassifierController object
+        """
+        
+        # Change label to ham or spam depending on prediction
         message = self.messageTextBox.toPlainText()
         prediction = self.model.predict(message)
         if prediction == 'ham':
@@ -23,5 +46,13 @@ class SpamClassifierController(QWidget):
             print("error")
     
     def resetScreen(self):
+        """
+        ui function to reset the screen after the
+        Reset button has been pressed
+        
+        :param self: SpamClassifierController - The SpamClassifierController object
+        """
+
+        # Reset text box and label
         self.messageTextBox.setText("")
         self.resultLabel.setText("")
--- Desktop/test_ui_spam_classifier_controller.py
+++ Desktop/test_ui_spam_classifier_controller.py
@@ -2,20 +2,22 @@ from PyQt6.QtCore import Qt
 from Controllers.spam_classifier_controller import SpamClassifierController
 
 def test_ui_spam_message(qtbot):
-    # 1. Instantiate and show your application's main window
+    """
+    A ui test to test if the right classification prediction appears
+    depending on the message provided
+    
+    :param qtbot: qtbot - automates the test process
+    """
+    
+    # Instantiate and show main window
     window = SpamClassifierController()
     qtbot.addWidget(window)
     window.show()
     qtbot.waitExposed(window)
 
-    # 2. Simulate user interactions using qtbot methods
+    # simulate user interactions using qtbot methods
     qtbot.keyClicks(window.messageTextBox, "Lol your always so convincing.")
-
-    # Example: Click a button named 'submit_button'
     qtbot.mouseClick(window.submitButton, Qt.MouseButton.LeftButton)
 
-    # 3. Add assertions to verify the application's state
+    # verify the application's state
     assert window.resultLabel.text() == "Ham"
-
-# if __name__ == '__main__':
-#     test_my_app_flow()   
